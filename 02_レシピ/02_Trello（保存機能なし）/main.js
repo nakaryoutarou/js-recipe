2774901e185358004418891c6e3ec40d377286c4
Nakaryoutarou <nakaryoutarou1126@gmail.com>
2023-04-23 20:13:01 +0900

    自販機

---
+++ 02_レシピ/02_Trello（保存機能なし）/main.js
@@ -0,0 +1,25 @@
+const cardscontainer = document.getElementById("cards-container")
+const inputtodo = document.getElementById("input-todo")
+const addbutton = document.getElementById("add-button")
+
+addbutton.onclick = function(){
+    //入力欄をとりだしてテxtにいれる
+    const text = inputElement.value
+
+    //card作成
+    const card = document.createElement("div")
+    card.className = "card"
+
+    //todo作成
+    const todo = document.createElement("todo")
+    todo.className = "todo"
+    todo.textContent = text
+
+    //todo をcardの中に追加する
+    card.append(todo)
+
+    //cardをcontainerのなかについかする
+    cardscontainer.append(card)
+
+    inputElement.value = ""
+}
